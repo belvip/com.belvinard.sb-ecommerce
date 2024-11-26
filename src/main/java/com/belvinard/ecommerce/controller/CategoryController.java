b2a948e7886cd3aa0cb269071d1ba5e610812d6f
belvip <pouadjeubelvi@gmail.com>
2024-11-26 16:59:59 +0100

    Adding Default Values

--- src/main/java/com/belvinard/ecommerce/controller/CategoryController.java
+++ src/main/java/com/belvinard/ecommerce/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.belvinard.ecommerce.controller;
 
 // Import statements
+import com.belvinard.ecommerce.config.AppConstants;
 import com.belvinard.ecommerce.model.Category; // Represents the Category model.
 import com.belvinard.ecommerce.payload.CategoryDTO;
 import com.belvinard.ecommerce.payload.CategoryResponse;
@@ -21,8 +22,8 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name = "pageNumber") Integer pageNumber,
-            @RequestParam(name = "pageSize") Integer pageSize) {
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize) {
         CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
