b50277fd4382020ada6fd557fc32849162797f78
belvip <pouadjeubelvi@gmail.com>
2024-12-05 05:00:02 +0100

    Implement Users

---
+++ src/main/java/com/belvinard/ecommerce/model/Address.java
@@ -0,0 +1,15 @@
+package com.belvinard.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    //private Integer roleId;
+
+}
